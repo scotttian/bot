f4955a3be0e20a2064f4e2ac1703dbb37e251623
weihetian <weihe@udel.edu>
2018-11-25 12:33:11 -0500

    new updates

--- MarkOne.py
+++ MarkOne.py
@@ -6,21 +6,31 @@ with open('spy.json') as f:
     data = json.load(f)
 
 last = None
+lastTwo = None
 for stock in data:
     if last != None:
         PremarketChange = round(stock['open'] - last['close'],4)
         PremarketChangePercentage = PremarketChange/last['close']
         previousChange = last['changePercent']
+        if lastTwo != None:
+            previousTwoChange = lastTwo['changePercent']
+        else:
+            previousTwoChange = 0
     else:
         PremarketChange = stock['open']
         PremarketChangePercentage =0
         previousChange = 0
+        if lastTwo != None:
+            previousTwoChange = lastTwo['changePercent']
+        else:
+            previousTwoChange = 0
     stock['PremarketChange'] = PremarketChange
     stock['PremarketChangePercentage'] = PremarketChangePercentage * 100
     stock['previousChange'] = previousChange
+    stock['previousTwoChange'] = previousTwoChange
     enriched.append(stock)
+    lastTwo = last
     last = stock
-
 
 def Stats(data,month):
     sameDirection = 0
@@ -31,10 +41,12 @@ def Stats(data,month):
         if index < offset:
             index = index+1
             continue
-        if abs(stock['PremarketChangePercentage'])<1.4:
+        if abs(stock['PremarketChangePercentage'])<0.8:
             #print stock['PremarketChangePercentage']
             continue
-        if abs(stock['previousChange'])<1.4:
+        if abs(stock['previousTwoChange'])<2:
+            continue
+        if abs(stock['previousChange'])<1.8:
             continue
         # if abs(stock['previousChange'])<1.5 or abs(stock['previousChange'])>2:
         #     continue
@@ -45,20 +57,24 @@ def Stats(data,month):
         if realChangePercent * stock['PremarketChangePercentage']>=0:
             #sameDirection =sameDirection+1
             #print stock['date']
-            netdiff = float(stock['changePercent'])-float(stock['PremarketChangePercentage'])
-            if abs(netdiff) > 0.5:
-                print stock['date']
-                print "premarket: " + str(stock['PremarketChangePercentage'])
+            #netdiff = float(stock['changePercent'])-float(stock['PremarketChangePercentage'])
+            if realChangePercent > 0.0:
+                print "----------UP-----------"
                 print "open: " + str(stock['open'])
                 print "close: " + str(stock['close'])
+                print "premarket: "+str(stock['PremarketChangePercentage'])
                 print "change percent: " + str(stock['changePercent'])
                 print "real change precent: "+str(realChangePercent)
-                print "net diff: " + str(netdiff)
+                #print "net diff: " + str(netdiff)
                 print "\n"
                 sameDirection =sameDirection+1
         else:
+            print "----------DOWN-----------"
             diffDirection = diffDirection+1
             print stock['date']
+            print "open: " + str(stock['open'])
+            print "close: " + str(stock['close'])
+            print "premarket: "+str(stock['PremarketChangePercentage'])
             print "real change precent: "+str(realChangePercent)
             print "\n"
             #print stock['PremarketChangePercentage']
